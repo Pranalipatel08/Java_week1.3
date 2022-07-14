d0026bdece1ef65c4966bf6c5b1bc44b51f42a1e
Pranalipatel08 <Pranalitcb@gmail.com>
2022-07-14 22:24:43 +0100

    Java_week1.3

---
+++ src/week1_3/Test_1.java
@@ -0,0 +1,9 @@
+package week1_3;
+
+public class Test_1 {
+    public static void main(String args[]){
+        for (int a = 0; a < 10; a++)
+            System.out.println("Ek Tha Tiger");
+
+    }
+}
---
+++ src/week1_3/Test_2.java
@@ -0,0 +1,14 @@
+package week1_3;
+
+public class Test_2 {
+    public static void main(String args[]){
+        String Name = "Tiger Abhi Zinda Hai";
+        int a = 0;
+        while (a<10) {
+            System.out.println(Name);
+            a++;
+        }
+
+
+    }
+}
